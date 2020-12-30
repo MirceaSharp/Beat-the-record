566138729db8985e703ce3496b6fea2e5b6cfce4
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2020-12-30 23:25:53 +0100

    adding files

---
+++ Beat the record/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beat_the_record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            a = 4.5;
+
+            Console.WriteLine(a);
+            while (a > 2)
+            {
+                Console.WriteLine("tijd is nog steeds >2min. try harder!");
+                a--;
+
+
+
+            }
+            Console.Write("Mircea Petrescu - new record " + a);
+            Console.ReadLine();
+        }
+    }
+}
